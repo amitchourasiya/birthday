0a0ba5459152669dc02aa2d7e2a6f8ddee619c3d
amitchourasiya <amitchourasiya@gmail.com>
2018-07-31 16:44:55 +0530

    Add files via upload

---
+++ Client.Shared/Infrastructure/IACMEInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACME.EmplyeeBirthdayWish.Client.Shared.Infrastructure
+{
+    public interface IACMEInitializer
+    {
+       // IEventAggregator EventAggregator { get; }
+    }
+}
